4a2a93119af6e5616790221b9610ecbd2bb904a0
Nikola Velkovski <nvelkovski@banzae.dev>
2023-03-09 16:43:22 +0100

    strapi-customization-ex1

---
+++ ex1/src/api/order/controllers/order.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/**
+ * order controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::order.order', ({strapi}) => (
+{
+    confirmOrder: async(ctx, next) => {
+        const {id} = ctx.request.params;
+        console.log(ctx.state.user)
+        await strapi.entityService.update("api::order.order", id, {
+            data: {
+                confirmed:true,
+                date_confirmed: new Date()
+            },
+        })
+      
+        return {
+            message: "confirmed",
+        }
+    },
+    async create(ctx, next){
+        const user = ctx.state.user
+        const order = await strapi.entityService.create("api::order.order", {
+            data: {
+                products: ctx.request.body.data.products,
+                owner: user.id
+            }
+        })
+        return {order}          
+    }
+   
+}
+ ));
